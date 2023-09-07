e9417c26b5dd52eda11f762a10c8b7a4819dba55
melquisedeque-magalhaes <melqui.sodre15@gmail.com>
2023-09-07 14:11:19 -0300

    adjust scroll to bottom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,32 @@ import { Input } from '@/components/ui/input'
 import { useChat } from 'ai/react'
 import { Header } from '@/components/ui/header'
 import { WelcomeChat } from '@/components/ui/welcomeChat'
+import { useEffect, useRef } from 'react'
 
 export default function Home() {
   const { messages, input, handleInputChange, handleSubmit } = useChat()
 
+  const chatContainerRef = useRef<null | HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      const { scrollHeight, clientHeight } = chatContainerRef.current
+
+      chatContainerRef.current.scrollTop = scrollHeight - clientHeight
+    }
+  }, [messages])
+
   return (
     <div className="h-full w-full">
-      <div className="h-full p-4 w-full max-w-[600px]  my-5 mx-auto flex flex-col">
+      <div className="h-full p-4 w-full max-w-[800px]  my-5 mx-auto flex flex-col">
         <Header.Root>
           <Header.Title>Chat Ninja</Header.Title>
         </Header.Root>
 
-        <div className="my-5 overflow-y-auto h-full max-h-[650px] p-4">
+        <div
+          ref={chatContainerRef}
+          className="my-5 overflow-y-auto h-full max-h-[650px] p-4"
+        >
           {messages.length === 0 && <WelcomeChat />}
 
           {messages.map((message) => (
